a751431dc1fb695dfc84369838fdfc52179b5ba1
sof-web <fikegaso814@gmail.com>
2025-10-03 04:28:53 +0300

    Update my.java

--- my.java
+++ my.java
@@ -1,1 +1,57 @@
-
+public class HashTable<K, V> {
+    private static class Entry<K, V> {
+        K key;
+        V value;
+        Entry<K, V> next;
+        
+        Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    private Entry<K, V>[] table;
+    private int capacity;
+    
+    @SuppressWarnings("unchecked")
+    public HashTable(int capacity) {
+        this.capacity = capacity;
+        this.table = new Entry[capacity];
+    }
+    
+    private int hash(K key) {
+        return Math.abs(key.hashCode()) % capacity;
+    }
+    
+    public void put(K key, V value) {
+        int index = hash(key);
+        Entry<K, V> newEntry = new Entry<>(key, value);
+        
+        if (table[index] == null) {
+            table[index] = newEntry;
+        } else {
+            Entry<K, V> current = table[index];
+            while (current.next != null) {
+                if (current.key.equals(key)) {
+                    current.value = value;
+                    return;
+                }
+                current = current.next;
+            }
+            current.next = newEntry;
+        }
+    }
+    
+    public V get(K key) {
+        int index = hash(key);
+        Entry<K, V> current = table[index];
+        
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+              }
